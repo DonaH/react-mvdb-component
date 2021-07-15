cef221501f3225923f3dbd513ff5480ea6cf84fb
d0nah <dona@gooduniversity.com>
2021-07-15 15:02:16 -0700

    Create Hero Image component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+
 // Components
+import HeroImage from './HeroImage';
 
 // Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
+
 // Image
 import NoImage from '../images/no_image.jpg';
 
@@ -14,7 +17,20 @@ const Home = () => {
 
   console.log(state);
 
-  return <div>Home Page</div>
-}
+  return (
+    // fragment use <> </>
+    <>
+      {state.results[0] ?
+        <HeroImage 
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        /> 
+        : null
+      }
+
+    </>
+  )
+};
 
 export default Home;
