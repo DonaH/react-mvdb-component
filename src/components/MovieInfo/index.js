c1826639f4f431fcf46cec9bf8a5be8fa98b22ab
d0nah <dona@gooduniversity.com>
2021-07-17 20:50:50 -0700

    Add session storage to Home fetch

--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -3,7 +3,6 @@ import Thumb from '../Thumb';
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
 import NoImage from '../../images/no_image.jpg';
 import { Wrapper, Content, Text } from './MovieInfo.styles';
-import Movie from '../Movie';
 import PropTypes from 'prop-types';
 
 
