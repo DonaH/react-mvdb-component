172e5ad5dd47ff6cb3ecf08515b66d4582b2898e
d0nah <dona@gooduniversity.com>
2021-07-22 21:49:33 -0700

    Refactor search bar with class component instead of hooks

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,46 +1,49 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 // Image
 import searchIcon from '../../images/search-icon.svg';
-
 // Styles
-import { Wrapper, Content  } from './SearchBar.styles';
+import { Wrapper, Content } from './SearchBar.styles';
 
-const SearchBar = ({ setSearchTerm }) => {
-    const [state, setState] = useState('');
-    const initial = useRef(true);
+class SearchBar extends Component {
+  state = { value: '' };
+  timeout = null;
 
-    useEffect(() => {
-        if (initial.current) {
-            initial.current = false;
-            return;
-        }
-        const timer = setTimeout(() => {
-            setSearchTerm(state);
-        }, 300)
+  componentDidUpdate(_prevProps, prevState) {
+    if (this.state.value !== prevState.value) {
+      const { setSearchTerm } = this.props;
 
-        return () => clearTimeout(timer)
-    },[setSearchTerm, state])
+      clearTimeout(this.timeout);
+
+      this.timeout = setTimeout(() => {
+        const { value } = this.state;
+        setSearchTerm(value);
+      }, 500);
+    }
+  }
+
+  render() {
+
+    const { value } = this.state;
 
     return (
-        <Wrapper>
-            <Content>
-                <img src={searchIcon} alt='search-icon' />
-                <input
-                    type='text'
-                    placeholder='Search Movie'
-                    onChange={event => setState(event.currentTarget.value)}
-                    value={state}
-                />
-            </Content>
-
-        </Wrapper>
-    )
-};
+      <Wrapper>
+        <Content>
+          <img src={searchIcon} alt='search-icon' />
+          <input
+            type='text'
+            placeholder='Search Movie'
+            onChange={event => this.setState({ value: event.currentTarget.value })}
+            value={value}
+          />
+        </Content>
+      </Wrapper>
+    );
+  }
+}
 
 SearchBar.propTypes = {
-    callback: PropTypes.func
+  callback: PropTypes.func
 };
 
 export default SearchBar;
